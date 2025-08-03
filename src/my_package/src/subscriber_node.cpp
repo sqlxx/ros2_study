8126591a0a70e23c50a951ca15957bcb997ebe8e
Sun Qin <sqlxx@163.com>
2025-08-03 15:38:16 +0800

    fix typo

--- src/my_package/src/subscriber_node.cpp
+++ src/my_package/src/subscriber_node.cpp
@@ -12,7 +12,7 @@ class MinimalSubscriber: public rclcpp::Node
     }
 
   private:
-    void topic_callback(cosnt std_msgs::msg::STring &msg) const
+    void topic_callback(const std_msgs::msg::String &msg) const
     {
       RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
     }
