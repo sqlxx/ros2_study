775b43fcf1ddea24b07285bfad278fbcead9747b
Sun Qin <sqlxx@163.com>
2025-08-03 15:26:31 +0800

    fix typo

--- src/my_package/src/publisher_node.cpp
+++ src/my_package/src/publisher_node.cpp
@@ -7,7 +7,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namesapce std::chrono_literals;
+using namespace std::chrono_literals;
 
 class MinimalPublisher : public rclcpp::Node
 {
